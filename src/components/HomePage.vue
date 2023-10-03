6ee38c2a1728f65d1967a9ffc2820dad3fc60b47
Lukasaurus11 <lucacuneob@gmail.com>
2023-10-03 16:41:43 +0200

    Updates

---
+++ src/components/HomePage.vue
@@ -0,0 +1,9 @@
+<!-- Home Page -->
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to the IE Bank</p>
+
+    <button type="button" class="btn btn-secondary">Accounts</button>
+  </div>
+</template>
